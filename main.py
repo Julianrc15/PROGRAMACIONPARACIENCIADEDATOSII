cc1ea9f2c36ae609cb73a41e110a2dc6ef30f352
Julian Rios <jarc2802@gmail.com>
2024-03-31 17:59:54 -0500

    final

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import pandas as pd
 from sklearn.metrics import roc_curve, auc
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from jupyter_dash import JupyterDash
 
-df_final= pd.read_csv("data\\datos_dashboard.csv")
+df_final = pd.read_csv("data\\datos_dashboard.csv")
 
-app = dash.Dash(__name__)
+app = JupyterDash(__name__)
 
 # Estilo del dropdown
 dropdown_style = {
     'width': '100%',
-    
 }
 
 # Definir el diseño del dashboard
 app.layout = html.Div([
     html.H1("Dashboard de Análisis Rentabilidad fondos de Inversión Colombianos", style={'background-color': '#581845', 'color': '#DAF7A6'}),
-    
+
     dcc.Dropdown(
         id='dropdown-categoria',
         options=[{'label': cat, 'value': cat} for cat in df_final['Categoría'].unique()],
@@ -29,7 +29,7 @@ app.layout = html.Div([
         placeholder="Seleccionar categoría",
         style=dropdown_style
     ),
-    
+
     dcc.Graph(id='rentabilidad-vs-volatilidad'),
     dcc.Graph(id='rentabilidad-diaria-histograma'),
     dcc.Graph(id='roc-curve'),
@@ -66,24 +66,24 @@ def update_datatable(selected_categoria):
 )
 def update_plots(selected_categoria):
     filtered_df = df_final[df_final['Categoría'] == selected_categoria]
-    
+
     # Crear un gráfico de dispersión de rentabilidad vs. volatilidad
     fig1 = px.scatter(filtered_df, x='Volatilidad', y='Rentabilidad diaria', 
                       color='Nombre del fondo', title='Rentabilidad vs. Volatilidad',
                       labels={'Volatilidad': 'Volatilidad', 'Rentabilidad diaria': 'Rentabilidad Diaria'})
-    
+
     # Crear un histograma de rentabilidad diaria
     fig2 = px.histogram(filtered_df, x='Rentabilidad diaria', 
                         title='Histograma de Rentabilidad Diaria',
                         labels={'Rentabilidad diaria': 'Rentabilidad Diaria'},
                         nbins=20)
-    
+
     # Calcular las tasas de verdaderos positivos (TPR) y las tasas de falsos positivos (FPR)
     fpr, tpr, thresholds = roc_curve(y_test, probs_class_1)
 
     # Calcular el área bajo la curva ROC (AUC)
     roc_auc = auc(fpr, tpr)
-    
+
     # Graficar la curva ROC
     roc_curve_fig = go.Figure()
     roc_curve_fig.add_trace(go.Scatter(x=fpr, y=tpr,
@@ -99,24 +99,23 @@ def update_plots(selected_categoria):
                                 yaxis_title='Tasa de Verdaderos Positivos (TPR)',
                                 xaxis=dict(range=[0, 1]),
                                 yaxis=dict(range=[0, 1]))
-    
+
     # Crear boxplot de rentabilidad vs. Volatilidad
     fig3 = px.box(filtered_df, x='Volatilidad', y='Rentabilidad diaria', 
                   title='Rentabilidad vs. Volatilidad',
                   labels={'Volatilidad': 'Volatilidad', 'Rentabilidad diaria': 'Rentabilidad Diaria'})
-    
+
     # Crear boxplot de rentabilidad vs. Benchmark
     fig4 = px.box(filtered_df, x='Benchmark', y='Rentabilidad diaria', 
                   title='Rentabilidad vs. Benchmark',
                   labels={'Benchmark': 'Benchmark', 'Rentabilidad diaria': 'Rentabilidad Diaria'})
-    
+
     # Crear boxplot de rentabilidad vs. Sharpe Ratio
     fig5 = px.box(filtered_df, x='Sharpe Ratio', y='Rentabilidad diaria', 
                   title='Rentabilidad vs. Sharpe Ratio',
                   labels={'Sharpe Ratio': 'Sharpe Ratio', 'Rentabilidad diaria': 'Rentabilidad Diaria'})
-    
+
     return fig1, fig2, roc_curve_fig, fig3, fig4, fig5
 
 # Ejecutar la aplicación Dash
-if __name__ == '__main__':
-    app.run(jupyter_mode="tab")
+app.run_server(mode='inline')
